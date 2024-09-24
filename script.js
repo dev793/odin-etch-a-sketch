b789759e4b6d83c34c50bcbd53a471512421c560
dev793 <117709900+dev793@users.noreply.github.com>
2024-09-24 16:41:38 +1200

    Add functionality to change canvas size

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function createGrid() {
-    for (let i = 0; i < 16; i++) {
+function createGrid(size) {
+    let rowHeight = PAGEHEIGHT / size;
+    console.log(rowHeight);
+
+    for (let i = 0; i < size; i++) {
         const rowContainer = document.createElement("div");
         rowContainer.setAttribute("class", "rowContainer");
+        rowContainer.style.height = rowHeight + "px";
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < size; j++) {
             const square = document.createElement("div");
             square.setAttribute("class", "square");
             rowContainer.appendChild(square);
@@ -13,11 +17,21 @@ function createGrid() {
     }
 }
 
-const container = document.querySelector("#container");
+function resetGrid() {
+    let size = Number(prompt("Enter a new gide size (1-100): ", "16"));
+    container.innerHTML = '';
+    createGrid(size);
+}
 
-createGrid();
+const container = document.querySelector("#container");
+const resetButton = document.querySelector("#reset");
+const PAGEHEIGHT = 800; //px
+
+createGrid(16);
 
 container.addEventListener("mouseover", (event) => {
     let target = event.target;
     target.setAttribute("style", "background: blue");
 });
+
+resetButton.addEventListener("click", resetGrid);
