1f075adf155d6f548630f3ed2410cc0da15c8c6a
dev793 <117709900+dev793@users.noreply.github.com>
2024-09-24 12:29:26 +1200

    Create grid layout

---
+++ script.js
@@ -0,0 +1,18 @@
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
+        const rowContainer = document.createElement("div");
+        rowContainer.setAttribute("class", "rowContainer");
+
+        for (let j = 0; j < 16; j++) {
+            const square = document.createElement("div");
+            square.setAttribute("class", "square");
+            rowContainer.appendChild(square);
+        }
+
+        container.appendChild(rowContainer);
+    }
+}
+
+const container = document.querySelector("#container");
+
+createGrid();
