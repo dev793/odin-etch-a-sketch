7900cbad5763945531bd2764ffdef071d27f398a
dev793 <117709900+dev793@users.noreply.github.com>
2025-12-28 10:15:45 +1300

    Create grid UI

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello world");
+function createGrid() {
+    const gridContainer = document.querySelector("#container");
+
+    for (let i = 0; i < 16; i++) {
+        createGridRow(gridContainer);
+    }
+}
+
+function createGridRow(parent) {
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("gridRow");
+
+    for (let i = 0; i < 16; i++) {
+        createGridCell(gridRow);
+    }
+
+    parent.append(gridRow)
+}
+
+function createGridCell(parent) {
+    const gridCell = document.createElement("div");
+    gridCell.classList.add("gridCell");
+    parent.append(gridCell);
+}
+
+createGrid();
